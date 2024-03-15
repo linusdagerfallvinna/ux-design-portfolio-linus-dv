93fe1c6e86dfbd6d292d5a517d94202894d80e95
linusdagerfallvinna <linus.dv97@gmail.com>
2024-03-15 16:53:09 +0100

    Test123
    Test

--- README.md
+++
@@ -1,4 +0,0 @@
-Link to GitHub Pages:
-https://linusdagerfallvinna.github.io/my_ux_portfolio/
-
-https://linusdagerfallvinnaa.github.io/ux-linus-vinna/
--- script.js
+++ script.js
@@ -21,6 +21,16 @@ $(document).ready(function () {
 });
 
 
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
 //Skapar skroll på sidan
 $(function () {
 
