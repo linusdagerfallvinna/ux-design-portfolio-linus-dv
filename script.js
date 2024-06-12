a6be3fa233a0605ba89ab5c142b80986b5abe0ce
linusdagerfallvinna <linus.dv97@gmail.com>
2024-06-12 14:43:12 +0200

    new changes

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-//Visar och döljer dropdown meny
-$(document).ready(function () {
 
-	$('#hamburger_menu_button').click(function () {
-		$('nav ul').toggleClass('showing');
-	});
-
-	$(".close").click(function () {
-		$('nav ul').toggleClass('showing');
-	});
-
-	$(document).on("click", function (e) {
-		if (
-			$(e.target).closest("nav ul").length == 0 &&
-			$("nav ul").hasClass("showing") &&
-			$(e.target).closest("#hamburger_menu_button").length == 0
-		) {
-			$('nav ul').toggleClass('showing');
-		}
-	});
-});
 
 
 function myFunction() {
@@ -34,17 +14,20 @@ function myFunction() {
 //Skapar skroll på sidan
 $(function () {
 
+	var x = document.getElementById("myLinks");
+
 	$('a[href*="#"]:not([href="#"])').click(function () {
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			x.style.display = "none";
 
 			if (target.length) {
 				$('html, body').animate({
 
-					scrollTop: target.offset().top
+					scrollTop: target.offset().top - 50
 
-				}, 1000);
+				}, 600);
 
 				return false;
 			}
